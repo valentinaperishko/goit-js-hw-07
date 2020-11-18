cbef90100caad916cb7781ca6a693bc6c99a3b8d
Valentina Perishko <valentina.perishko@gmail.com>
2020-11-18 14:07:39 -0600

    changes

--- task-6.js
+++ task-6.js
@@ -1,19 +1,23 @@
-document.getElementById("validation-input").onblur = function() {
-    console.log(this.value.length);
-    
-    if (this.getAttribute("data-length") === this.value.length) { 
-      this.classList.remove("invalid");
-      this.classList.add("valid");
-      
-    } 
-    if (this.value.length !== this.getAttribute('data-length') && this.value.length !== 0) {
-      this.classList.add("invalid");
-    
-    }
-    if (this.value.length === 0) {
-      this.classList.remove("valid");
-      this.classList.remove("invalid");
-    }
-  };
+
+const input = document.getElementById('validation-input');
+// console.log(input);
+input.addEventListener('input', (event) => {
+// console.log(input.value.length);
+// console.log(event.target.dataset.length);
+let inputLength = event.target.value.length;
+let dataLength = Number(event.target.dataset.length);
+if (inputLength === dataLength) {
+  input.classList.remove('invalid');
+  input.classList.add('valid');
+} else if (inputLength === 0) {
+  input.classList.remove('invalid');
+  input.classList.remove("valid");
+
+} else {
+  input.classList.remove('valid');
+  input.classList.add('invalid');
+
+}
+});
 
 
