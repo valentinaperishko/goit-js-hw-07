d9385384988e88c391d84d5a0d2688cfce99946d
Valentina Perishko <valentina.perishko@gmail.com>
2020-11-18 10:27:33 -0600

    adds changes

--- task-6.js
+++ task-6.js
@@ -1,10 +1,19 @@
 document.getElementById("validation-input").onblur = function() {
     console.log(this.value.length);
-    if (this.getAttribute('data-length') === this.value.length) { 
-      this.classList.remove('invalid');
-      this.classList.add('valid');
-    } else {
-      this.classList.remove('valid');
-      this.classList.add('invalid');
+    
+    if (this.getAttribute("data-length") === this.value.length) { 
+      this.classList.remove("invalid");
+      this.classList.add("valid");
+      
+    } 
+    if (this.value.length !== this.getAttribute('data-length') && this.value.length !== 0) {
+      this.classList.add("invalid");
+    
+    }
+    if (this.value.length === 0) {
+      this.classList.remove("valid");
+      this.classList.remove("invalid");
     }
   };
+
+
